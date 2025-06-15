e13883acf60466d8938602e6dcca77ce88e5d2ce
Andrzej Prochyra <andrzej@prochyra.io>
2025-06-15 12:43:44 +0100

    add toggle button that shows some text

--- CoreAudioBeep/ContentView.swift
+++ CoreAudioBeep/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Play") {
+                showDetails.toggle()
+            }
+            if showDetails {
+                Text("Foo")
+            }
         }
         .padding()
     }
