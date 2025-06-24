9c386ccbe638c887018c32e1dd21c0733e3b9865
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2025-06-24 18:53:03 +0100

    Restructuring tests

--- CoreAudioBeepTests/CoreAudioBeepTests.swift
+++ CoreAudioBeepTests/CoreAudioBeepTests.swift
@@ -13,6 +13,7 @@ struct CoreAudioBeepTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        #expect(true)
     }
 
 }
