1103e8e28fc9844385e50505dd4ba19ad6ead9ce
Petar Simonovic <petar@simonovic.com>
2025-07-10 18:58:56 +0100

    Add test shell for WhiteNoise generation

---
+++ CoreAudioBeepTests/WhiteNoiseTests.swift
@@ -0,0 +1,13 @@
+import Testing
+import Foundation
+
+@testable import CoreAudioBeep
+
+struct WhiteNoiseTests {
+    
+    // test that there is a WhiteNoise class that we can instantiate an instance of
+    // test that we can ask it for nextSample
+    // when we ask it for nextSample, it asks its random number generator for a sample
+    
+    
+}
